3b9d59635cb43f7caca8101549091d01cf5cd839
DUHUM3 <aalzarigy@gmail.com>
2025-04-10 21:18:18 +0300

    add firebase messages

--- Routes/adminRoutes.js
+++ Routes/adminRoutes.js
@@ -8,16 +8,16 @@ const authMiddleware = require("../middleware/authAdminMiddleware");
 // تسجيل الدخول (Login)
 router.post("/login", async (req, res) => {
     try {
-        const { username, password } = req.body;
-        const admin = await Admin.findOne({ username });
-        
+        const { email, password } = req.body;
+        const admin = await Admin.findOne({ email });
+
         if (!admin) return res.status(404).json({ message: "Admin not found" });
-        
+
         const isMatch = await bcrypt.compare(password, admin.password);
         if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
-        
+
         const token = jwt.sign({ id: admin._id }, "SECRET_KEY", { expiresIn: "1h" });
-        res.json({ token, admin: { id: admin._id, username: admin.username } });
+        res.json({ token, admin: { id: admin._id, email: admin.email } });
     } catch (error) {
         res.status(500).json({ message: "Server error" });
     }
@@ -26,14 +26,14 @@ router.post("/login", async (req, res) => {
 // إنشاء حساب إداري (Register)
 router.post("/register", async (req, res) => {
     try {
-        const { username, password } = req.body;
-        const existingAdmin = await Admin.findOne({ username });
-        if (existingAdmin) return res.status(400).json({ message: "Username already exists" });
-        
+        const { email, password } = req.body;
+        const existingAdmin = await Admin.findOne({ email });
+        if (existingAdmin) return res.status(400).json({ message: "Email already exists" });
+
         const hashedPassword = await bcrypt.hash(password, 10);
-        const newAdmin = new Admin({ username, password: hashedPassword });
+        const newAdmin = new Admin({ email, password: hashedPassword });
         await newAdmin.save();
-        
+
         res.status(201).json({ message: "Admin created successfully" });
     } catch (error) {
         res.status(500).json({ message: "Server error" });
